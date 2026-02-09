f15a9c7bccd0a677dee2198e1f1724671599db7d
Bright Kpobi <kpobibright010@gmail.com>
2026-02-09 19:26:04 +0000

    done-2

--- app/(auth)/login/page.tsx
+++
@@ -1,4 +0,0 @@
-// src/app/(auth)/login/page.tsx
-export default function LoginPage() {
-    return <h1 className="text-xl font-bold">Login</h1>;
-}
--- app/(dashboard)/dashboard/orders/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function DashboardOrders() {
-    return <h1 className="text-xl font-bold">Orders</h1>;
-}
